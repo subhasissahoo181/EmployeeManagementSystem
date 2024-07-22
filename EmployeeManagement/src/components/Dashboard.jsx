836e6d4d4cebe86a4a2959914b179a77a553b56f
Subhasis <subhasissahoo181@gmail.com>
2024-07-22 09:38:08 +0530

    first commit

--- EmployeeManagement/src/components/Dashboard.jsx
+++ EmployeeManagement/src/components/Dashboard.jsx
@@ -13,35 +13,45 @@ function Dashboard() {
                 <ul className='nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-item' id='menu'>
                     <li className='w-100'>                    
                     <Link to="/dashboard" className='nav-link text-white px-0 align-middle'>
+                    <i className="fs-4 bi-speedometer2 ms-2"></i>
                     <span className='ms-2  d-none d-sm-inline'>Dashboard</span>
                     </Link>
                     </li>
 
                     <li className='w-100'>                    
                     <Link to="/dashboard" className='nav-link text-white px-0 align-middle'>
+                    <i className="fs-4 bi-people-fill ms-2"></i>
                     <span className='ms-2  d-none d-sm-inline'>Create Employee</span>
                     </Link>
                     </li>
 
                      <li className='w-100'>                    
                     <Link to="/dashboard" className='nav-link text-white px-0 align-middle'>
+                    <i className='fs-4 bi-people ms-2'></i>
                     <span className='ms-2  d-none d-sm-inline'>EmployeeList</span>
                     </Link>
                     </li>   
 
                     <li className='w-100'>                    
                     <Link to="/dashboard" className='nav-link text-white px-0 align-middle'>
+                    <i className="fs-4 bi-person ms-2"></i>
                     <span className='ms-2  d-none d-sm-inline'>Profile</span>
                     </Link>
                     </li>                      
                     
                     <li className='w-100'>                    
                     <Link to="/dashboard" className='nav-link text-white px-0 align-middle'>
+                    <i className="fs-4 bi-power ms-2"></i>
                     <span className='ms-2  d-none d-sm-inline'>Logout</span>
                     </Link>
                     </li>
 
                 </ul>
+            </div>
+        </div>
+        <div className='col p-0 m-0'>
+            <div className='p-2 d-flex justify-content-center shadow'>
+                <h4>Welcome Admin Panel</h4>
             </div>
         </div>
     </div>
